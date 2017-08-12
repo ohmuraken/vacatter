a5ea1ec92b2c132a9c5617e41cc360b35ce9c3ef
ShotaOnishi <showta.0804@gmail.com>
2017-08-12 13:16:00 +0900

    いいねボタンの設置

--- android/presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/MainActivity.java
+++ android/presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.fernandocejas.android10.sample.presentation.view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -13,6 +15,7 @@ import com.fernandocejas.android10.sample.presentation.R;
 public class MainActivity extends BaseActivity {
 
   @Bind(R.id.btn_LoadData) Button btn_LoadData;
+  @Bind(R.id.btn_LoadTimeTine) Button btn_LoadTimeLine;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +30,11 @@ public class MainActivity extends BaseActivity {
   @OnClick(R.id.btn_LoadData)
   void navigateToUserList() {
     this.navigator.navigateToUserList(this);
+  }
+
+  @OnClick(R.id.btn_LoadTimeTine)
+  public void submit(View view) {
+    Intent intent = new Intent(MainActivity.this, TimeLineActivity.class);
+    startActivity(intent);
   }
 }
--- android/presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/TimeLineActivity.java
+++ android/presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/TimeLineActivity.java
@@ -5,9 +5,9 @@ import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 import com.fernandocejas.android10.sample.presentation.R;
 
 /**
@@ -22,15 +22,24 @@ public class TimeLineActivity extends BaseActivity {
     cardLinear.removeAllViews();
     for (int i = 0; i < 5; i++) {
       LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-      LinearLayout linearLayout = (LinearLayout) inflater.inflate(R.layout.test_card, null);
+      LinearLayout linearLayout = (LinearLayout) inflater.inflate(R.layout.row_card, null);
       CardView cardView = (CardView) linearLayout.findViewById(R.id.cardView);
       TextView textBox = (TextView) linearLayout.findViewById(R.id.textBox);
+      Button favBtn = (Button) linearLayout.findViewById(R.id.btn_favorite);
+
       textBox.setText("CardView" + i);
       cardView.setTag(i);
+      favBtn.setTag(i);
       cardView.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-          Log.d("CLICKED_NUMBER",String.valueOf(v.getTag()));
+          Log.d("CLICKED_NUMBER", String.valueOf(v.getTag())+"の詳細ページへのリンク");
+        }
+      });
+      favBtn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          Log.d("CLICKED_NUMBER", String.valueOf(v.getTag()) + "　に「いいね！」が押されました");
         }
       });
       cardLinear.addView(linearLayout, i);
