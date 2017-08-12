59eda59fdf38da23ce90a028a2f798cf464c8e03
Matsumoto Kazutaka <paper.sheet.kami@gmail.com>
2017-08-13 02:40:04 +0900

    add Face class in domain
    (cherry picked from commit 6a5d4ab)

---
+++ android/domain/src/test/java/com/fernandocejas/android10/sample/domain/interactor/PostFaceTest.kt
@@ -0,0 +1,33 @@
+package com.fernandocejas.android10.sample.domain.interactor
+
+import com.fernandocejas.android10.sample.domain.executor.PostExecutionThread
+import com.fernandocejas.android10.sample.domain.executor.ThreadExecutor
+import com.fernandocejas.android10.sample.domain.repository.FaceRepository
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class PostFaceTest {
+  lateinit var mockThreadExecutor: ThreadExecutor
+  lateinit var mockPostExecutionThread: PostExecutionThread
+  lateinit var mockFaceRepository: FaceRepository
+  lateinit var postFace: PostFace
+
+  @Before
+  fun setUp() {
+    mockThreadExecutor = Mockito.mock(ThreadExecutor::class.java)
+    mockPostExecutionThread = Mockito.mock(PostExecutionThread::class.java)
+    mockFaceRepository = Mockito.mock(FaceRepository::class.java)
+    postFace = PostFace(mockFaceRepository, mockThreadExecutor, mockPostExecutionThread)
+  }
+
+  @Test
+  fun testPostFaceUseCaseObserverHappyCase() {
+    postFace.buildUseCaseObservable(null)
+
+    Mockito.verify(mockFaceRepository).face()
+  }
+}
