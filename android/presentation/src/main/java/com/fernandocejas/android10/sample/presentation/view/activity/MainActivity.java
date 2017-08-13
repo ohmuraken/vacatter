e0ada95a102cb8d96f64071a81c5bee8080178e6
AnnaYoshioka <anna@d172-196.jaist.ac.jp>
2017-08-13 10:23:00 +0900

    Twitter login completed

--- android/presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/MainActivity.java
+++ android/presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/MainActivity.java
@@ -3,6 +3,7 @@ package com.fernandocejas.android10.sample.presentation.view.activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.View;
 import android.provider.Settings;
 
@@ -33,38 +34,29 @@ public class MainActivity extends BaseActivity {
   private TwitterLoginButton loginButton;
   @Bind(R.id.btn_LoadData) Button btn_LoadData;
   @Bind(R.id.btn_LoadTimeTine) Button btn_LoadTimeLine;
-  @Bind(R.id.btn_LoginTwitter)
-  Button btn_LoginTwitter;
+  @Bind(R.id.btn_LoginTwitter) TwitterLoginButton btn_LoginTwitter;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    loginButton = (TwitterLoginButton) findViewById(R.id.btn_LoginTwitter);
-    setContentView(R.layout.activity_main);
-    TwitterConfig config = new TwitterConfig.Builder(this)
-        .logger(new DefaultLogger(Log.DEBUG))
-        .twitterAuthConfig(new TwitterAuthConfig("CONSUMER_KEY", "CONSUMER_SECRET"))
+    TwitterConfig config = new TwitterConfig.Builder(this).logger(new DefaultLogger(Log.DEBUG))
+        .twitterAuthConfig(new TwitterAuthConfig("UZ07BQX9FRvXK6f0pkRPkQr0D","Dw7Dl7w0y0Je5YO07DwjukTliQMazE0sxKfZUnc5PHXovoZjXy"))
         .debug(true)
         .build();
-    Twitter.initialize(this);
+    Twitter.initialize(config);
+    setContentView(R.layout.activity_main);
     ButterKnife.bind(this);
 
-    /**loginButton.setCallback(new Callback<TwitterSession>() {
-      @Override
-      public void success(Result<TwitterSession> result) {
-        // Do something with result, which provides a TwitterSession for making API calls
-        //TwitterSession session = result.data;
-        //String msg = "@" + session.getUserName() + " logged in! (#" + session.getUserId() + ")";
-        //Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+    btn_LoginTwitter.setCallback(new Callback<TwitterSession>() {
+      @Override public void success(Result<TwitterSession> result) {
+        TwitterSession session = result.data;
+        String msg = "@" + session.getUserName() + "logged in! (#" + session.getUserId()+ ")";
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
       }
 
-      @Override
-      public void failure(TwitterException exception) {
-        // Do something on failure
-        //Log.d("TwitterKit", "Login with Twitter failure", exception);
+      @Override public void failure(TwitterException exception) {
+        Log.d("TwitterKit", "Login with Twitter failure", exception);
       }
     });
-     **/
-
   }
 
   /**
@@ -82,9 +74,8 @@ public class MainActivity extends BaseActivity {
   @Override
   protected void onActivityResult(int requestCode, int resultCode, Intent data) {
     super.onActivityResult(requestCode, resultCode, data);
-    // Make sure that the loginButton hears the result from any
-    // Activity that it triggered.
-    loginButton.onActivityResult(requestCode, resultCode, data);
+
+    btn_LoginTwitter.onActivityResult(requestCode, resultCode, data);
     }
 
 }
