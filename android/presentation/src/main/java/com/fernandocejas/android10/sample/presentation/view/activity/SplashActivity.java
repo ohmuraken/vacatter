8354be315c4a394250bd08d465a5bb194597f206
ShotaOnishi <showta.0804@gmail.com>
2017-08-14 23:08:35 +0900

    初回起動時のチュートリアル画面への遷移実装

--- android/presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/SplashActivity.java
+++ android/presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/SplashActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.AppLaunchChecker;
 import android.support.v7.app.AppCompatActivity;
 import com.fernandocejas.android10.sample.presentation.R;
 
@@ -15,25 +16,34 @@ import com.fernandocejas.android10.sample.presentation.R;
 public class SplashActivity extends BaseActivity{
   private Handler handler = new Handler();
   private Runnable execute_intent;
+  private Intent intent;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_splash);
+    final boolean status=AppLaunchChecker.hasStartedFromLauncher(getApplicationContext());
+    AppLaunchChecker.onActivityCreate(this);
+
 
     execute_intent = new Runnable() {
       @Override
       public void run() {
-        Intent intent;
+
         SharedPreferences prefer = getSharedPreferences("twitter", Context.MODE_PRIVATE);
         String token = prefer.getString("token", "");
-        if (!token.equals("")) {
-          intent = new Intent(SplashActivity.this, TimeLineActivity.class);
-        } else {
-          intent = new Intent(SplashActivity.this, MainActivity.class);
+        if(status){
+          if (!token.equals("")) {
+            intent = new Intent(SplashActivity.this, TimeLineActivity.class);
+          } else {
+            intent = new Intent(SplashActivity.this, MainActivity.class);
+          }
+        }else{
+          intent = new Intent(SplashActivity.this, TutorialActivity.class);
         }
         startActivity(intent);
         finish();
+
 
       }
     };
---
+++ android/presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/TutorialActivity.java
@@ -0,0 +1,16 @@
+package com.fernandocejas.android10.sample.presentation.view.activity;
+
+import android.os.Bundle;
+import com.fernandocejas.android10.sample.presentation.R;
+
+/**
+ * Created by apple on 2017/08/14.
+ */
+
+public class TutorialActivity extends BaseActivity{
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_tutorial);
+  }
+}
