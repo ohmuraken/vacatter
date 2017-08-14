3a2160904d5f980af33d02c6c46acb2bd4787632
ShotaOnishi <showta.0804@gmail.com>
2017-08-14 20:23:34 +0900

    shared preferencesにcontextを追加

--- android/presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/MainActivity.java
+++ android/presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends BaseActivity {
         String msg = "@" + session.getUserName() + "logged in! (#" + session.getUserId() + ")";
         Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
 
-        SharedPreferences prefer = getSharedPreferences("twitter", MODE_PRIVATE);
+        SharedPreferences prefer = getSharedPreferences("twitter", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = prefer.edit();
         editor.putString("token", session.getAuthToken().token);
         editor.apply();
