b8801c5680901e78e2cb71772631f820e87c4530
ShotaOnishi <showta.0804@gmail.com>
2017-08-16 02:38:25 +0900

    timelineから写真撮影画面への導線作成

--- android/presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/TimeLineActivity.java
+++ android/presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/TimeLineActivity.java
@@ -1,6 +1,8 @@
 package com.fernandocejas.android10.sample.presentation.view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -21,6 +23,15 @@ public class TimeLineActivity extends BaseActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_time_line);
     LinearLayout cardLinear = (LinearLayout) this.findViewById(R.id.cardLinear);
+    FloatingActionButton registBtn = (FloatingActionButton) findViewById(R.id.btn_pic_regist);
+    registBtn.setOnClickListener(new View.OnClickListener(){
+      @Override
+      public void onClick(View v) {
+        Intent intent = new Intent(TimeLineActivity.this, MainCameraActivity.class);
+        startActivity(intent);
+        finish();
+      }
+    });
     cardLinear.removeAllViews();
     for (int i = 0; i < 5; i++) {
       LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
