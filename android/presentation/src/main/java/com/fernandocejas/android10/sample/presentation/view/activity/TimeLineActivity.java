d1a82065fa1068b0dd606e0119427536f90ea1cc
Matsumoto Kazutaka <paper.sheet.kami@gmail.com>
2017-08-16 03:04:46 +0900

    refactor & merge

--- android/presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/TimeLineActivity.java
+++ android/presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/TimeLineActivity.java
@@ -1,49 +1,38 @@
 package com.fernandocejas.android10.sample.presentation.view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import com.bumptech.glide.Glide;
+import android.support.design.widget.FloatingActionButton;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import com.fernandocejas.android10.sample.presentation.R;
-import com.fernandocejas.android10.sample.presentation.view.fragment.SquareHeightFragment;
+import com.fernandocejas.android10.sample.presentation.internal.di.HasComponent;
+import com.fernandocejas.android10.sample.presentation.internal.di.components.DaggerUseCaseComponent;
+import com.fernandocejas.android10.sample.presentation.internal.di.components.UseCaseComponent;
+import com.fernandocejas.android10.sample.presentation.view.fragment.TweetCardFragment;
 
 public class TimeLineActivity extends BaseActivity implements HasComponent<UseCaseComponent> {
 
   private UseCaseComponent usecaseComponent;
+  @Bind(R.id.btn_PicRegister) FloatingActionButton btn_PicRegist;
 
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_time_line);
-    LinearLayout cardLinear = (LinearLayout) this.findViewById(R.id.cardLinear);
-    FloatingActionButton registBtn = (FloatingActionButton) findViewById(R.id.btn_pic_regist);
-    registBtn.setOnClickListener(new View.OnClickListener(){
-      @Override
-      public void onClick(View v) {
-        Intent intent = new Intent(TimeLineActivity.this, MainCameraActivity.class);
-        startActivity(intent);
-        finish();
-      }
-    });
-    cardLinear.removeAllViews();
-    for (int i = 0; i < 5; i++) {
-      LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-      LinearLayout linearLayout = (LinearLayout) inflater.inflate(R.layout.row_card, null);
-      SquareHeightFragment imageView = (SquareHeightFragment)linearLayout.findViewById(R.id.post_image);
-      Glide.with(this).load("http://goo.gl/h8qOq7").into(imageView);
-      CardView cardView = (CardView) linearLayout.findViewById(R.id.cardView);
-      TextView textBox = (TextView) linearLayout.findViewById(R.id.textBox);
-      Button favBtn = (Button) linearLayout.findViewById(R.id.btn_favorite);
+    ButterKnife.bind(this);
 
     // Generate Object Tree
     this.initializeInjector();
     if (savedInstanceState == null) {
       addFragment(R.id.fragmentContainer, new TweetCardFragment());
     }
+  }
+
+  @OnClick(R.id.btn_PicRegister)
+  void clickPicRegister() {
+    Intent intent = MainCameraActivity.getCallingIntent(getApplicationContext());
+    startActivity(intent);
   }
 
   private void initializeInjector() {
