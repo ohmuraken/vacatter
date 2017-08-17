942c676379083d22cd7490657fffd258090b599c
ohmuraken <ohmurakendev@gmail.com>
2017-08-17 19:18:54 +0900

    顔交換URLの生成処理 #29

--- server/app/main.py
+++ server/app/main.py
@@ -8,11 +8,14 @@ from flask import Flask, jsonify, abort, redirect
 from flask import g, request, Response, flash,  url_for
 from werkzeug.utils import secure_filename
 from flask import send_from_directory
+import numpy as np
+import cv2
 
 
 UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'uploads/')
 FACE_UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'your_face/')
 FACE_CHANGE_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'face_change/')
+FACE_CASCADE_DATA = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data/haarcascade_frontalface_default.xml')
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 consumer_key = "UZ07BQX9FRvXK6f0pkRPkQr0D"
@@ -26,6 +29,7 @@ DATABASE = os.path.join(dir_path, 'database.sqlite3')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['FACE_UPLOAD_FOLDER'] = FACE_UPLOAD_FOLDER
 app.config['FACE_CHANGE_FOLDER'] = FACE_CHANGE_FOLDER
+app.config['FACE_CASCADE_DATA'] = FACE_CASCADE_DATA
 
 
 def allowed_file(filename):
@@ -46,11 +50,50 @@ def get_db():
     return db
 
 
-def make_face_change(media_urls):
-    pass # TODO
+# def url_to_image(url):
+#     f = io.BytesIO(urllib.request.urlopen(url).read())
+#     img = Image.open(f)
+#     return img
+def url_to_image(url):
+    resp = urllib.request.urlopen(url)
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    return image
+
+def make_face_change(user_id, media_urls, face_url, face_cascade):
+    print("=============")
+    face_change_urls = []
+    print("face_url: {}".format(face_url))
+    face_path = os.path.join(app.config["FACE_UPLOAD_FOLDER"], face_url.rsplit("/")[-1])
+    face_img = cv2.imread(face_path)
+    print("try to get face")
+    for media_url in media_urls:
+        if media_url != "none":
+            base_img = url_to_image(media_url)
+            base_img_gray = cv2.cvtColor(base_img, cv2.COLOR_BGR2GRAY)
+            base_faces = face_cascade.detectMultiScale(base_img_gray, 1.3, 5)
+            print("media_url: {}".format(media_url))
+            print("base_face size: {}".format(len(base_faces)))
+            if len(base_faces): # 顔が検出できた
+                for (x, y, w, h) in base_faces:
+                    # 顔交換
+                    base_img[y:y+h, x:x+w] = cv2.resize(face_img, (h, w))
+                    # 保存
+                    filename = str(user_id) + "_" + media_url.rsplit("/", 1)[-1]
+                    # file.save(os.path.join(app.config['FACE_CHANGE_FOLDER'], filename))
+                    cv2.imwrite(os.path.join(app.config['FACE_CHANGE_FOLDER'], filename), base_img)
+                    face_change_url = os.path.join("http://", request.host, "face_change", filename)
+                    face_change_urls.append(face_change_url)
+                return face_change_urls
+            else: # 顔が検出できない
+                face_change_urls.append("none")
+                # face_change_urls.append(media_url) # こっちでもいいかも
+        else:
+            face_change_urls.append("none")
+    return face_change_urls
 
 
-def remake_json(tweet):
+def remake_json(tweet, face_url):
     # tweet内の画像のURLを取得する.
     if "extended_entities" in tweet._json.keys(): # 画像がある(最大4枚)
         media_urls = [media["media_url_https"] for media in tweet._json["extended_entities"]["media"]]
@@ -82,10 +125,23 @@ def remake_json(tweet):
 
     # 顔変換 TODO
     face_change_count = 0
-    face_change_urls = ["none", "none", "none", "none"]
-    # face_change_urls = make_face_change(media_urls) # TODO
+
+    if media_urls.count("none") != 4:
+        print("check face")
+        face_cascade =  cv2.CascadeClassifier(app.config['FACE_CASCADE_DATA'])
+        print(type(face_cascade))
+        face_change_urls = make_face_change(tweet._json["user"]["id"], media_urls, face_url, face_cascade) # TODO
+    else:
+        print("NO check face")
+        face_change_urls = ["none", "none", "none", "none"]
+
+
+    print("media_urls: {}".format(media_urls))
+    print("face_change_urls: {}".format(face_change_urls))
 
     remaked_json = {
+        "tweet_id": tweet._json["id"],
+        "user_id": tweet._json["user"]["id"],
         "name": tweet._json["user"]["name"],
         "screen_name": tweet._json["user"]["screen_name"],
         "text": tweet._json["text"],
@@ -138,12 +194,19 @@ def user_timeline():
     c.execute(sql, (user_id,))
     result = c.fetchall()
     if len(result):
-        user_id, access_token, acceess_token_secret = result[0][:-1]
+        user_id, access_token, acceess_token_secret, face_url = result[0]
+        print("face_url: {}".format(face_url))
         conn.commit()
         conn.close()
         api = authorize(consumer_key, consumer_secret, access_token, access_token_secret)
         tweets = api.home_timeline(count=100)
-        res = [remake_json(tweet) for tweet in tweets if "extended_entities" in tweet._json.keys()]
+        # res = [remake_json(tweet, face_url) for tweet in tweets if "extended_entities" in tweet._json.keys()]
+        res = []
+        for tweet in tweets:
+            if "extended_entities" in tweet._json.keys():
+                remaked_json = remake_json(tweet, face_url)
+                if remaked_json["face_change_urls"].count("none") != 4:
+                    res.append(remaked_json)
         return jsonify(res)
     else:
         return "no such user"
@@ -204,8 +267,13 @@ def face_url():
 
 
 @app.route('/your_face/<filename>', methods=['GET'])
-def uploaded_file(filename):
+def uploaded_face(filename):
     return send_from_directory(app.config['FACE_UPLOAD_FOLDER'], filename)
+
+
+@app.route('/face_change/<filename>', methods=['GET'])
+def uploaded_face_change(filename):
+    return send_from_directory(app.config['FACE_CHANGE_FOLDER'], filename)
 
 
 @app.teardown_appcontext
