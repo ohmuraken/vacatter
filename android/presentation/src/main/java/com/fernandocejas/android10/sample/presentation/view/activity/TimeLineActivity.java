82ac110199e39779be3cc94220e486a07ac8e991
ShotaOnishi <showta.0804@gmail.com>
2017-08-18 01:19:24 +0900

    popup実装

--- android/presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/TimeLineActivity.java
+++ android/presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/TimeLineActivity.java
@@ -1,13 +1,23 @@
 package com.fernandocejas.android10.sample.presentation.view.activity;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import com.fernandocejas.android10.sample.presentation.R;
 import com.fernandocejas.android10.sample.presentation.internal.di.HasComponent;
+
 import com.fernandocejas.android10.sample.presentation.internal.di.components.DaggerUseCaseComponent;
 import com.fernandocejas.android10.sample.presentation.internal.di.components.UseCaseComponent;
 import com.fernandocejas.android10.sample.presentation.view.fragment.TweetCardFragment;
@@ -16,6 +26,7 @@ public class TimeLineActivity extends BaseActivity implements HasComponent<UseCa
 
   private UseCaseComponent usecaseComponent;
   @Bind(R.id.btn_PicRegister) FloatingActionButton btn_PicRegist;
+  @Bind(R.id.bac_dim_layout) RelativeLayout backDimLayout;
 
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -31,8 +42,45 @@ public class TimeLineActivity extends BaseActivity implements HasComponent<UseCa
 
   @OnClick(R.id.btn_PicRegister)
   void clickPicRegister() {
-    Intent intent = MainCameraActivity.getCallingIntent(getApplicationContext());
-    startActivity(intent);
+    PopupWindow popupWin;
+    RelativeLayout back_dim_layout = (RelativeLayout) findViewById(R.id.bac_dim_layout);
+    back_dim_layout.setVisibility(View.VISIBLE);
+    LinearLayout popLayout
+        = (LinearLayout)getLayoutInflater().inflate(
+        R.layout.popup_window, null);
+    Drawable drawable = getResources().getDrawable(R.drawable.popup_background);
+    popupWin = new PopupWindow(this){
+      @Override
+      public void dismiss(){
+        super.dismiss();
+        backDimLayout.setVisibility(View.GONE);
+      }
+    };
+
+    Button cameraButton = (Button)popLayout.findViewById(R.id.btn_camera);
+    Button galleryButton = (Button)popLayout.findViewById(R.id.btn_gallery);
+
+    cameraButton.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        Intent intent = MainCameraActivity.getCallingIntent(getApplicationContext());
+        startActivity(intent);
+        Log.d("CLICKED_BUTTON","camera!");
+      }
+    });
+    galleryButton.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        //ギャラリー選択時の処理
+        Log.d("CLICKED_BUTTON","gallery!");
+      }
+    });
+
+    popupWin.setWindowLayoutMode(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+    popupWin.setContentView(popLayout);
+    popupWin.setBackgroundDrawable(drawable);
+    popupWin.setOutsideTouchable(true);
+    popupWin.setFocusable(true);
+    popupWin.setAnimationStyle(R.style.animationName);
+    popupWin.showAtLocation(popLayout, Gravity.CENTER, 0, 0);
   }
 
   private void initializeInjector() {
